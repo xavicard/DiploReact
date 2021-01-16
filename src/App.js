5463b4f8996fa2aad04d557c17094c011cb0a7c2
xavi <xavi.cardenasd@gmail.com>
2021-01-15 21:40:58 -0400

    clase 15/01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Cabecera from './Cabecera/Cabecera';
 
 function App(props) {
-  const [edad,setEdad]=React.useState(30)
-  const [nombre,setNombre]=React.useState(props.nombre);
-  let mensaje;
-  const cambiarEdad = (edad,nombre) => {
+  const [persona,actPersona]=React.useState({
+    edad:0,
+    nombre:'',    
+  })
+//  const [edad,setEdad]=React.useState(30)
+//  const [nombre,setNombre]=React.useState(props.nombre);
+  let mensaje='';
+/*  const cambiarEdad = (edad,nombre) => {
     setEdad(edad)
-    setNombre(nombre)} 
-  if(edad>=18){
+    setNombre(nombre)} */
+  const cambiaValor = e =>{
+    //alert(e.target.name)
+    actPersona({
+      ...persona,
+      [e.target.name]:e.target.value
+    })
+  }
+  const [errorEnvio,activaError]= React.useState(false)
+
+  const enviarDatos = e =>{
+    e.preventDefault();
+    if(persona.nombre.trim()==='' || persona.edad==0 || persona.edad<0 || persona.edad>120 )
+    {
+      activaError(true);
+      return;
+    }
+    activaError(false);
+    alert('Enviado Correctamente');
+  }
+  if(persona.edad>=18){
     mensaje=<h1>Es mayor de edad</h1>
   }
   else
@@ -18,14 +41,16 @@ function App(props) {
   return (
     <div className="App">
       <Cabecera 
-        nombre={nombre}
-        edad={edad}
+        nombre={persona.nombre}
+        edad={persona.edad}
       />
+      <form onSubmit={enviarDatos}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {mensaje} {nombre}
+          {mensaje} {persona.nombre} Edad: {persona.edad}
         </p>
+        {errorEnvio ? <p>Los campos son obligatorios</p> : null }        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -34,10 +59,17 @@ function App(props) {
         >
           Learn React
         </a>
-        <button type="button" id="boton" onClick={ () => cambiarEdad('17','Juan Perez') }>
-          Inicio
+        <label>Nombre</label>
+        <input type="text" name="nombre" onChange={cambiaValor}>
+        </input>
+        <label>Edad</label>
+        <input type="number" name="edad" onChange={cambiaValor}>
+        </input>
+        <button type="submit" id="boton"  >
+          Guardar
         </button>
       </header>
+      </form>
     </div>
   );
 }
